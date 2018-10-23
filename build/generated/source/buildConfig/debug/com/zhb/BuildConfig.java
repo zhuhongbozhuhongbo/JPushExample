824f7a0257ad488939ae4b9532d105b8c18e03a2
朱宏博 <1076306278@qq.com>
2018-10-23 22:59:58 +0800

    Initial commit

---
+++ build/generated/source/buildConfig/debug/com/zhb/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zhb;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zhb";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 315;
+  public static final String VERSION_NAME = "3.1.5";
+}
